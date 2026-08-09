47452c55a3a93475d4f6b917aa68f8b31ae2050c
anganasubhash <anganasubhash003@gmail.com>
2026-08-09 14:31:36 +0530

    parking

---
+++ control flow/iterative statement/While/task_while/ques20.py
@@ -0,0 +1,9 @@
+#20. Parking Area
+#Cars enter the parking lot until all 50 spaces are occupied
+
+space=50
+while space>0:
+    print("parking lot available",space)
+    space-=1
+
+print("Housefull")
