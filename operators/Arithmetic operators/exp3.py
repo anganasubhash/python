11b976a6353c4dafcb039e1f72e7b2ea2d5d7bea
anganasubhash <anganasubhash003@gmail.com>
2026-08-09 21:51:36 +0530

    intro

---
+++ operators/Arithmetic operators/exp3.py
@@ -0,0 +1,7 @@
+#Find area of the rectangle
+#length and breath is 10,5
+
+length=10
+breath=5
+Area=length*breath
+print("Area of the rectangle",Area)
---
+++ operators/Arithmetic operators/inro.py
@@ -0,0 +1,12 @@
+#   ARTHIMETIC OPERATORS
+#---------------------------->
+
+# 1.ADDITION(+)
+# 2.SUBATRACTION(-)
+# 3.DIVISION(/)
+# 4.FLOOR DIVISON(//)------>quioteint
+# 5.MODULUS(%)----------> reminder
+# 6.EXPONENT(**)-------->raise to
+# 7,MULTIPLICATION(*)
+# 
+# 
