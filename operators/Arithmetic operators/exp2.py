e9ec6c0fb4d771ea34a4813e874ea8396a85d339
anganasubhash <anganasubhash003@gmail.com>
2026-08-09 21:51:56 +0530

    example

---
+++ operators/Arithmetic operators/exp2.py
@@ -0,0 +1,8 @@
+#input num1,num2,num3
+#do addition and multiplication
+
+num1=int(input("Enter the first number:"))
+num2=int(input("Enter the  second number:"))
+num3=int(input("Enter the third number:"))
+print("sum of " ,num1,",",num2,",",num3,", is",num1+num2+num3)
+print("multiplication of",num1,",",num2,",",num3,", is",num1*num2*num3)
