243210b0f7d9977b8d43f3a320f8cc7faf6281ff
anganasubhash <anganasubhash003@gmail.com>
2026-08-11 16:00:14 +0530

    comparison

---
+++ 5.operators/comparison operator/exam2.py
@@ -0,0 +1,7 @@
+age=20
+print(age<18)
+print(age>18)
+print(age<=18)
+print(age>=18)
+print(age!=18)
+print(age==18)
---
+++ 5.operators/comparison operator/intro.py
@@ -0,0 +1,8 @@
+#COMPARISON OPERATORS
+#---------------------------
+#1.Less than(<)
+#2.Greater than(>)
+#3.Less than or equal to(=<)
+#4.Greater than or equal to(=>)
+#5.Not equal (!=)
+#6.Equal to(=)
