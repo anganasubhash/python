1d4ce39263d5e7d38987b894bed597104dcef3ae
anganasubhash <anganasubhash003@gmail.com>
2026-08-11 07:23:13 +0530

    data

---
+++ 1.identifiers/identifers.py
@@ -0,0 +1,5 @@
+#programming elements that can identify
+#Name given to program elements
+#Eg;variable ,Function,classes.........
+#num=10(This is a variable )
+# def    (This is a function)
---
+++ 2.Data/Data.py
@@ -0,0 +1,40 @@
+#Data=set of infromatiom/facts(Name,Place,Age.....etc)
+#Big data------>#High volume(Huge amount of data) 
+#               ----------------------------------
+#                      kb,mb, gb,tb,pb,eb,zb,yb
+#                     social media---40yb/day
+#
+#               #High velocity(speed of data)
+#                         speed of data (in on every refresh variety of data is given)
+#
+#               #High variety(images, audio,video,....etc)
+#150 tb data stored in  a system is it a big data?-------NO
+#  High volume ------True
+#  High variety-------True
+#  High speed---------False (only stored data will given on every use)
+
+#Type of programing language
+#----------------------------
+
+#Strictly typed programming language
+#         Data type  mentioned in variable eariler
+#          int age;
+#                age=40------>True
+#                age="fourty"---> not true here
+#         Eg;c+,c++,java
+
+#Dynamic typed programming language
+#       not need to mention 
+#        Eg; python,R,Javascript
+
+#Varibles-----> To assign a value or store a value
+Age=20
+#
+#Data type; 
+#        Integers-->12,98,45,1000(Normal numbers)
+#        Long------>947677389849638900
+#        Float---->38.90,8.7,78(Decimals)
+#        Double---->6.897768789,4.7645356767
+#        String,Character,variable character----> "Ammu"-----> words
+#
+#
