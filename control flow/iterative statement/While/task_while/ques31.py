586ead8a0e9eb17c9c33bfcb9bce2204f32d2626
anganasubhash <anganasubhash003@gmail.com>
2026-08-11 07:38:39 +0530

    folder renaming

---
+++ control flow/iterative statement/While/task_while/ques31.py
@@ -0,0 +1,7 @@
+#31. Banking Menu
+#Display the following menu repeatedly:
+#Deposit
+# Withdraw
+# Check Balance
+#Exit
+#Continue until the user selects Exit
--- identifers/identifers.py
+++
@@ -1,5 +0,0 @@
-#programming elements that can identify
-#Name given to program elements
-#Eg;variable ,Function,classes.........
-#num=10(This is a variable )
-# def    (This is a function)
