f09c31ad13587aaaee38d6fef4852f60d5a8ec1f
anganasubhash <anganasubhash003@gmail.com>
2026-08-20 20:43:53 +0530

    mutable

---
+++ 8.collections/List/list4.py
@@ -0,0 +1,9 @@
+#mutable or unmutable
+#[10,20,30,40]
+
+#2------>add new element 100 
+#mutable----->can change or add element its mutable
+#30----->100
+list=[10,20,30,40]
+list[2]=100
+print(list)
