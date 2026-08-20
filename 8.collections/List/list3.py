46e11698fc1aed54d25b203103bd7fac2d4857a6
anganasubhash <anganasubhash003@gmail.com>
2026-08-20 20:31:38 +0530

    list3

--- 8.collections/List/list3.py
+++ 8.collections/List/list3.py
@@ -5,3 +5,7 @@
 lst1=[10,20,30]
 print(lst1)
 #List is preserved
+#For preserved ----- there is index
+#[10,20,30,40,50,60,70,80,90,100]
+#Forward index------(0 to n-1)
+#Backward index-------(-1 to-n)
