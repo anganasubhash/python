97e149efbe4fa17c629ab54a025155fc3e55678b
anganasubhash <anganasubhash003@gmail.com>
2026-08-20 10:13:57 +0530

    list26

---
+++ 8.collections/List/list26.py
@@ -0,0 +1,14 @@
+lst=[['vinay',30,'DS',30000],['keerthi',23,"DA",45000],['aryan',35,"SW",50000]]
+for i in lst:
+    if i[1]>25:
+     print(i)
+
+
+for i in lst:
+   if i[2]=="DA":
+      print(i)
+
+for i in lst:
+   if i[0]=="aryan":
+      print(i[1:])
+
