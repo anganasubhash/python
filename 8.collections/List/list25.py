815e1ab7cea380d4abac87f035e282eb1c232435
anganasubhash <anganasubhash003@gmail.com>
2026-08-20 09:42:11 +0530

    25 change

--- 8.collections/List/list25.py
+++ 8.collections/List/list25.py
@@ -3,6 +3,7 @@
 lst34=['abc','hjh','cac','fds','sfgh','pop','yty','ret','ere','121','678','67856']
 #hjh,cac,pop,yty,ere,121,454-----same starting and ending
 #[]
+#taking cac as i then 0==-1
 #for---0 to n-1
 #back---1 to -n
 list=[]
