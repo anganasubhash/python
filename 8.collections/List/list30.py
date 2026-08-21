374f47d128daab99294d478d6dd9cf0283d8fb6e
anganasubhash <anganasubhash003@gmail.com>
2026-08-21 10:13:53 +0530

    list30

---
+++ 8.collections/List/list30.py
@@ -0,0 +1,14 @@
+#Slicing
+#to get subportions
+#[start:Stop:step]
+
+lst=[10,20,67,56,89,43,23]
+print(lst[3:5])
+
+#[10,67,56,43]
+#[89,43,23]
+print(lst[4:])
+print(lst[0:7:2])
+print(lst[::-1])
+#[43,56,20]
+print(lst[-2::-2])
