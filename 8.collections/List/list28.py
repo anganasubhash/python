da19bbb2f196fe3da0c89309b09ef7c86182c07e
anganasubhash <anganasubhash003@gmail.com>
2026-08-21 08:59:12 +0530

    list28

---
+++ 8.collections/List/list28.py
@@ -0,0 +1,19 @@
+ 
+lst12=["apple","orange","banana"]
+#papaya
+#append---------->Add
+lst12.append("papaya")
+print(lst12)
+#extend-------->Add mulitiple element
+lst12.extend(["kiwi","Avocado"])
+print(lst12)
+#insert------>index location based
+lst12.insert(1,"mango")
+print(lst12)
+
+
+#REMOVE-------to remove elements
+#ist12.remove("name of element to remove")
+lst12.remove("banana")
+print(lst12)
+
