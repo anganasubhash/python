e20e8fb8cfbdee977409d23f5e437ae908a9aeb0
anganasubhash <anganasubhash003@gmail.com>
2026-08-31 21:06:35 +0530

    dic

--- 8.collections/Dictionary/intro.py
+++ 8.collections/Dictionary/intro.py
@@ -19,6 +19,32 @@ print(dit_name)"""
 
 }
 print(student)"""
+#Differnt dayatype as values
+#---------------------------
+
+
+'''student={
+    "Name":"Priya",#String
+    "Age" :20,#int
+    "Mark":67.1,#float
+    "Passed":True,#bool
+    "Subject":["Python","Java","c++","c"],#list
+    "passed subject":("python","c"),#Tuple
+    "Department":{2,3,4}#set
+
+
+}
+print(student)'''
+
+#Different dataype as key
+"""student={
+    "Name":"Anju",
+    90:"Mark",
+    97.8:"Percentage",
+    True :1
+}
+print(student)"""
+
 
 
 
@@ -163,7 +189,7 @@ print(student)"""
 
 
 #check whether a key exist,using membership operators-age
-student={
+"""student={
     "Name":"Priya",
     "Age" :20,
     "Place":"Thrissur",
@@ -172,4 +198,4 @@ student={
 print("Age" in student)
 print("Age" not in student)
 print("num" in student)
-print("num" not in student)
+print("num" not in student)"""
