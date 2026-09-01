f34dfc333931951bca6b870b1367a71fe4a5da30
anganasubhash <anganasubhash003@gmail.com>
2026-09-01 10:01:36 +0530

    collection

--- 8.collections/intro.py
+++ 8.collections/intro.py
@@ -13,3 +13,9 @@
 #duplicate /allowed or not
 #preserved/unpreserved------>it has index print same as we given
 #mutable/unmutable
+
+
+#list---[]              hetro    duplicate allowed        preserved      mutable
+#tuple --()             hetro    duplicate allowed        preserved      unmutable
+#set-----set()          hetro     no duplicate            unperservd     mutable
+#dictonary-{key:value}  hetro    key---not,value--alllow   perserved      mutable
