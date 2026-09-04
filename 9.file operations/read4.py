5b54d80e11339a7f960070812608be5889202328
anganasubhash <anganasubhash003@gmail.com>
2026-09-04 08:23:43 +0530

    read

--- 9.file operations/read4.py
+++ 9.file operations/read4.py
@@ -1,5 +1,20 @@
-#numbers 200 to 211
-#add in to list
-#add even numbers
-#add odd number
-#sum
+#number 200-211
+
+
+file3=open("number1","r")
+number=[]
+even_number=[]
+odd_number=[]
+for i in file3:
+    number.append(int(i.rstrip()))
+
+    if int(i)%2==0:
+        even_number.append(int(i.rstrip()))
+    else:
+        odd_number.append(int(i.rstrip()))
+print(number)
+print(even_number)
+print(odd_number)
+print(sum(number))
+print(sum(even_number))
+print(sum(odd_number))
